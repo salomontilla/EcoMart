5dde083db4812d6e90a88f4b4c8fa14d4adecc9d
salo <salomontilla@outlook.com>
2025-01-17 01:05:14 -0500

    add personalized chat configs

--- src/main/java/com/curses/ecomart/controller/ProductCategorizer.java
+++ src/main/java/com/curses/ecomart/controller/ProductCategorizer.java
@@ -1,22 +1,29 @@
 package com.curses.ecomart.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.chat.prompt.ChatOptionsBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/categorizer")
 public class ProductCategorizer {
     private final ChatClient chatClient;
     public ProductCategorizer(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
+
     @GetMapping
-    public String generate() {
-        var inputUser = "generate 5 ideas of eco products";
+    public String generate(String product) {
+        var system = "you are a product categorizer";
         return this.chatClient.prompt()
-                .user(inputUser)
+                .system(system)
+                .user(product)
+                .options(ChatOptionsBuilder.builder()
+                        .withTemperature(0.82)
+                        .build())
                 .call()
                 .content();
     }
